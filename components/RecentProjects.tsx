4dbcfc6763111a198b8ccc8db41397f870486c59
so-bit <65305015+so-bit@users.noreply.github.com>
2025-02-24 23:20:49 -0800

    Update RecentProjects.tsx Image import

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { projects } from '../data'
 import { PinContainer } from './ui/3d-pin'
 import { FaLocationArrow } from 'react-icons/fa'
+import Image from "next/image";
+
 
 const RecentProjects = () => {
   return (
@@ -17,9 +19,9 @@ const RecentProjects = () => {
                     <PinContainer title={link} href={link}>
                         <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h[40vh] h-[30vh] mb-10'>
                             <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]' >
-                                <img src="/bg.png" alt="bg-img" />
+                                <Image src="/bg.png" alt="bg-img" />
                             </div>
-                            <img
+                            <Image
                              src={img}
                              alt={title}
                              className="z-10 absolute bottom-0"
@@ -38,7 +40,7 @@ const RecentProjects = () => {
                                 {iconLists.map((icon,index)=>(
                                     <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center style={{
                                     transform: `translateX(-${5*index*2}px`}}'>
-                                        <img src={icon} alt={icon}
+                                        <Image src={icon} alt={icon}
                                         className='p-2' />
                                     </div>  
                                     
