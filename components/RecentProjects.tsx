48851cdcc42190c6ceb9253cd52f5be81757da62
so-bit <65305015+so-bit@users.noreply.github.com>
2025-02-24 22:46:29 -0800

    Update RecentProjects.tsx data import relative path

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { projects } from './data'
+import { projects } from '../data'
 import { PinContainer } from './ui/3d-pin'
 import { FaLocationArrow } from 'react-icons/fa'
 
