0e4306db8654dcbcbabbf12b9b6384e7d07f470c
so-bit <65305015+so-bit@users.noreply.github.com>
2025-02-24 22:51:56 -0800

    Update Experience.tsx ../data

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { workExperience } from './data'
+import { workExperience } from '../data'
 import { Button } from './ui/MovingBorders'
 
 const Experience = () => {
