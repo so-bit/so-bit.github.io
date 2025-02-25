1fc5ecf7ab913aee7756d7ff73260756742b12cb
so-bit <65305015+so-bit@users.noreply.github.com>
2025-02-24 23:18:52 -0800

    Update Footer.tsx Image import

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
 import { socialMedia } from '../data'
+import Image from "next/image";
+
 
 const Footer = () => {
   return (
@@ -30,7 +32,7 @@ const Footer = () => {
             <div className='flex items-center md:gap-3 gap-6'>
                 {socialMedia.map((info)=>(
                     <div key={info.id} className='w-10 h-10 cursur-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opaity-75 bg-black-200 rounded-lg border border-black-300'>
-                        <img src={info.img} alt="icons"
+                        <Image src={info.img} alt="icons"
                         width={20} height={20} 
                     />
                         </div>
